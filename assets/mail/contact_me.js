501b46ebf4b6a7506d0376e2019feebc4889ec30
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-10-24 13:09:43 +0200

    debugging contact prob 7

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,8 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                //url: "{https://68hm28fwje.execute-api.us-east-1.amazonaws.com/default/lamda_func}",
-                url: "{https://9vby3n8xoa.execute-api.eu-west-1.amazonaws.com/default/Cloud_Computing}",
+                url: "{https://ocjvr4e0ac.execute-api.eu-west-1.amazonaws.com/default/lambda_portfolio}",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),

30999e8f6b41fde6f6f4f35825121ee500139cf4
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-10-26 11:20:06 +0200

    DEBUGING 1

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://itdl31nzaf.execute-api.eu-west-1.amazonaws.com/default/mytest",
+                url: "https://jsr32aqol8.execute-api.eu-west-1.amazonaws.com/default/my_lambda",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
